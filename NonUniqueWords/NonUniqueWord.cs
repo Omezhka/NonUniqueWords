ad491c74c06170027a1bc469df0285d3f9b8f744
Наталья Аверкова <cynopterussphinx@gmail.com>
2022-09-07 17:14:19 +0300

    add files

---
+++ NonUniqueWords/NonUniqueWord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonUniqueWords
+{
+    public class NonUniqueWord
+    {
+        public static string NonUniqueWordCount(string str)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            string strRes = "";
+
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                var strSplit = str.Split(' ');
+
+                foreach (var word in strSplit)
+                {
+                    if (!dic.ContainsKey(word))
+                        dic.Add(word, 1);
+                    else
+                        dic[word] += 1;
+                }
+
+                foreach (var word in dic)
+                {
+                    if (word.Value > 1)
+                        strRes += $"{word.Key}: {word.Value}\n";
+                }
+            }
+            return strRes;
+        }
+    }
+}
---
+++ NonUniqueWords/Program.cs
@@ -0,0 +1,4 @@
+﻿using NonUniqueWords;
+
+var str = "London Great is a Britain capital London of Great capital Britain London London";
+Console.WriteLine(NonUniqueWord.NonUniqueWordCount(str));
